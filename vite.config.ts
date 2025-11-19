9dd911cb6e3e5d3ba7149bcdae57616142d830ba
Prathat2006 <prathmeshhatwar2006@gmail.com>
2025-11-19 20:34:50 +0530

    fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: '/TexMind-Editor/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
